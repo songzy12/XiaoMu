79d8f4852cb345b3a553a8b6093d983fd75e7da2
songzy <songzy.thu@qq.com>
2019-03-10 20:19:15 +0800

    data util for matchzoo

--- src/util.py
+++ src/util.py
@@ -29,7 +29,7 @@ def save(json_data, filename):
 
 def prepare_data():
     text2id = {}
-    
+
     label_file = '../data/label/match.json'
     candidate_file = '../data/label/candidate.json'
 
@@ -39,47 +39,68 @@ def prepare_data():
     with io.open(candidate_file, encoding='utf8') as f:
         m_candidate = json.loads(f.read())
 
-    relation = []
     left = []
     right = []
+    train_relation = []
+    test_relation = []
 
     num_candidate = 5
-    
+
+    test_portion = 0.1
+    cnt = int((1-test_portion)*len(m_candidate))
+    cur_cnt = 0
+
     for k, v in m_candidate.items():
-        
+        cur_cnt += 1
+
         if k not in m_match or not m_match[k]:
             continue
         if k not in text2id:
             text2id[k] = 't_%d' % (len(text2id))
             left.append([text2id[k], k])
-        
+
         for _candidate in v[:num_candidate]:
             candidate = _candidate[0]
 
             if candidate not in text2id:
                 text2id[candidate] = 't_%d' % (len(text2id))
                 right.append([text2id[candidate], candidate])
+
             if candidate in m_match[k]:
-                relation.append([text2id[k], text2id[candidate], 1])
+                if cur_cnt < cnt:
+                    train_relation.append([text2id[k], text2id[candidate], 1])
+                else:
+                    test_relation.append([text2id[k], text2id[candidate], 1])
             else:
-                relation.append([text2id[k], text2id[candidate], 0])
-        
+                if cur_cnt < cnt:
+                    train_relation.append([text2id[k], text2id[candidate], 0])
+                else:
+                    test_relation.append([text2id[k], text2id[candidate], 0])
+
         for _candidate in v[num_candidate:]:
             candidate = _candidate[0]
             if candidate not in text2id:
                 text2id[candidate] = 't_%d' % (len(text2id))
                 right.append([text2id[candidate], candidate])
-            if candidate in m_match[k]:
-                relation.append([text2id[k], text2id[candidate], 1])
 
-    relation = pd.DataFrame(relation, columns=['id_left', 'id_right', 'label'])
+            if candidate in m_match[k]:
+                if cur_cnt < cnt:
+                    train_relation.append([text2id[k], text2id[candidate], 1])
+                else:
+                    test_relation.append([text2id[k], text2id[candidate], 1])
+
+    train_relation = pd.DataFrame(train_relation, columns=['id_left', 'id_right', 'label'])
+    test_relation = pd.DataFrame(test_relation, columns=['id_left', 'id_right', 'label'])    
     left = pd.DataFrame(left, columns=['id_left', 'text_left'])
     left.set_index('id_left', inplace=True)
     right = pd.DataFrame(right, columns=['id_right', 'text_right'])
     right.set_index('id_right', inplace=True)
 
-    data_pack = DataPack(relation=relation, left=left, right=right)
-    dirpath = '../data'
+    data_pack = DataPack(relation=train_relation, left=left, right=right)
+    dirpath = '../data/train'
+    data_pack.save(dirpath)
+    data_pack = DataPack(relation=test_relation, left=left, right=right)
+    dirpath = '../data/test'
     data_pack.save(dirpath)
 
 
