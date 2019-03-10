9572b23ccc0d9f6e9eaf5f02400cec60a36450de
songzy <songzy.thu@qq.com>
2019-03-10 21:31:11 +0800

    delete MatchZoo old

--- MatchZoo/data/xiaomu/run.sh
+++
@@ -1,1 +0,0 @@
-python3 train_word2vec_model.py texts texts.100.model texts.100.vector
--- MatchZoo/examples/xiaomu/run.sh
+++
@@ -1,5 +0,0 @@
-./run_arci_classification.sh
-./run_arcii_classification.sh
-./run_duet_classification.sh
-./run_matchpyramid_classification.sh
-./run_mvlstm_classification.sh
